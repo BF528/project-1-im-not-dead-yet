14702ddedcf59742875cd2ffdc0cc1a4add40523
Jason Yeung <jy357@scc-wi3.scc.bu.edu>
2022-02-11 14:08:57 -0500

    Updated preprocessing (4) with median histograms

--- preprocessing.R
+++ preprocessing.R
@@ -3,6 +3,7 @@ library(affyPLM)
 library(sva)
 library(AnnotationDbi)
 library(hgu133plus2.db)
+library(ggplot2)
 
 filepath = '/project/bf528/project_1/data/GSE39582/CEL_files'
 
@@ -18,8 +19,31 @@ rma(celbatch)
 # convert AffyBatch to PLMset
 pset <- fitPLM(celbatch, normalize = TRUE, background = TRUE)
 
-# relative log expression
+
+# relative log expression (RLE)
 rle_stats <- data.frame(t(affyPLM::RLE(pset, type='stats')))
+
+# plot rle_stats
+rle_medians <- ggplot(rle_stats, aes(x=median)) + 
+  geom_histogram(bins=50, 
+                 color = 'dodgerblue4', 
+                 fill = 'white') +
+  labs(title = 'RLE Medians') +
+  theme_classic()
+rle_medians
+
 
 # normalized unscaled standard errors (NUSE)
 nuse_stats <- data.frame(t(NUSE(pset, type = 'stats')))
+
+# plot nuse_stats
+nuse_medians <- ggplot(nuse_stats, aes(x=median)) + 
+  geom_histogram(bins=50, 
+                 color = 'dodgerblue4', 
+                 fill = 'white') +
+  labs(title = 'NUSE Medians') +
+  theme_classic()
+nuse_medians
+
+
+
