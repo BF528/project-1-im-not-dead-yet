69737b1ab0e80a6adf2fd6e42e872f7156c42569
Jason Yeung <jy357@scc-wi3.scc.bu.edu>
2022-02-11 12:58:52 -0500

    Updated through (preprocessing 4)) RLE and NUSE

--- preprocessing.R
+++ preprocessing.R
@@ -4,4 +4,22 @@ library(sva)
 library(AnnotationDbi)
 library(hgu133plus2.db)
 
-path_to_CELs = '/project/bf528/project_1/data/GSE39582/CEL_files'
+filepath = '/project/bf528/project_1/data/GSE39582/CEL_files'
+
+celpath <- system.file('celfiles', package='affydata')
+fns <- list.celfiles(path=celpath, full.names=TRUE)
+
+# read in files
+celbatch <- ReadAffy(celfile.path=filepath)
+
+# normalize files together
+rma(celbatch)
+
+# convert AffyBatch to PLMset
+pset <- fitPLM(celbatch, normalize = TRUE, background = TRUE)
+
+# relative log expression
+rle_stats <- data.frame(t(affyPLM::RLE(pset, type='stats')))
+
+# normalized unscaled standard errors (NUSE)
+nuse_stats <- data.frame(t(NUSE(pset, type = 'stats')))
